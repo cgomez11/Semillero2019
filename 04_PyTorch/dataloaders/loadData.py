46f9629cac2cc513c2eab028240bb24dec8933fb
Catalina Gómez <c.gomez10@uniandes.edu.co>
2019-06-27 21:47:18 -0500

    Compose transform and visualization

--- 04_PyTorch/dataloaders/loadData.py
+++ 04_PyTorch/dataloaders/loadData.py
@@ -2,13 +2,14 @@
 
 # Tutorial for loading your own data
 import numpy as np
+import matplotlib.pylab as plt
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-from torchvision import datasets, transforms, models
+from torchvision import datasets, transforms, models, utils
 
-from dataloader import myDataset
+from dataloader import myDataset, Rescale
 
 # define batch size
 batch_size = 8
@@ -25,11 +26,11 @@ labels = np.load('labels_ids.npy').item()
 data_path =  './hymenoptera_data/'
 
 # call your dataset function
-train_dataset = myDataset(partition['train'], labels, data_path + 'train/', transform=transforms.Compose([transforms.ToTensor()]))
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+train_dataset = myDataset(partition['train'], labels, data_path + 'train/', transform = transforms.Compose([Rescale((375, 500)), transforms.ToTensor()]))
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, shuffle = True, num_workers = 4)
 
-val_dataset = myDataset(partition['val'], labels, data_path + 'val/', transform=transforms.Compose([transforms.ToTensor()]))
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+val_dataset = myDataset(partition['val'], labels, data_path + 'val/', transform = transforms.Compose([Rescale((375, 500)), transforms.ToTensor()]))
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = batch_size, shuffle = True, num_workers = 4)
 
 # check the size of your datatset
 dataset_sizes = {}
@@ -43,6 +44,23 @@ for batch_idx, (inputs, targets) in enumerate(train_loader):
     print('Batch: {}, shape input: {}, label: {}'.format(batch_idx, inputs.shape, targets.data))
 
 
+# Visualize training images
+'''
+class_names = ['bees', 'ants']
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
 
 
+# Get a batch of training data
+inputs, classes = next(iter(train_loader))
 
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
